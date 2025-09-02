7e582d3c7fac0b4a7f768f52abd2722e24256e39
LI,WEI-MING <workonly1022@gmail.com>
2025-09-02 13:44:08 +0800

    new cors

--- server.js
+++ server.js
@@ -9,9 +9,23 @@ const app = express();
 const PORT = 3000;
 app.use(cors({
     origin: [
-        'http://127.0.0.1:5503', // 允許您的 VS Code Live Server
-        'http://localhost:5503'  // 同上，有時 IP 會解析為 localhost        
+        'http://127.0.0.1:5503',
+        'http://localhost:5503',
+        'https://www.tripleswav.com'
     ]
+}));
+app.use(cors({
+    origin: function (origin, callback) {
+        // 允許沒有 origin 的請求 (例如來自 Postman 或伺服器自身的請求)
+        if (!origin) return callback(null, true);
+
+        // 如果請求的來源在我們的白名單中，就允許它
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = '此來源的 CORS 政策不允許存取: ' + origin;
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
 }));
 app.use(express.json());
 
